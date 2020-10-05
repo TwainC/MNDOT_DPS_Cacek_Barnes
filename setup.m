67f7abab34f7e1ed3757694f3ba4d21b2da7971c
TwainC <72367465+TwainC@users.noreply.github.com>
2020-10-04 22:22:44 -0500

    Update setup.m

--- setup.m
+++ setup.m
@@ -1,1 +1,2 @@
-fred = '/Users/TwainCacekHD/Desktop/Grad School (2020-2021)/Research/Data/TH002_L1_2020-07-28_RMD2_RawData/TH002_L1_2020-07-28_rdm2__018.csv';
+folder = '/Users/TwainCacekHD/Desktop/Grad School (2020-2021)/Research/Data/TH002_L1_2020-07-28_RMD2_RawData';
+file = '/Users/TwainCacekHD/Desktop/Grad School (2020-2021)/Research/Data/TH002_L1_2020-07-28_RMD2_RawData/TH002_L1_2020-07-28_rdm2__021.csv';
