447ad4ce1eca1d61d175283a01824da7e9426d26
TwainC <72367465+TwainC@users.noreply.github.com>
2020-10-19 18:54:40 -0500

    Update setup.m
    added additional test data

--- setup.m
+++ setup.m
@@ -1,3 +1,5 @@
 folder = '/Users/TwainCacekHD/Desktop/Grad School (2020-2021)/Research/Data/TH002_L1_2020-07-28_RMD2_RawData';
 lonFile = '/Users/TwainCacekHD/Desktop/Grad School (2020-2021)/Research/Data/TH002_L1_2020-07-28_RMD2_RawData/TH002_L1_2020-07-28_rdm2__021.csv';
 swerveFile = '/Users/TwainCacekHD/Desktop/Grad School (2020-2021)/Research/Data/TH002_L1_2020-07-28_RMD2_RawData/TH002_L1_sw_2020-07-28_rdm2__026.csv';
+allData = '/Users/TwainCacekHD/Desktop/Grad School (2020-2021)/Research/Data/Presentation Data';
+notLon = '/Users/TwainCacekHD/Desktop/Grad School (2020-2021)/Research/Data/Presentation Data/TH002_L1_2020-07-28_RMD2_RawData/TH002_L1_2020-07-28_rdm2__015.csv';
