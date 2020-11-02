22cb8b01687980c84e3a32bb033a3f2e047d9c78
TwainC <72367465+TwainC@users.noreply.github.com>
2020-11-02 10:49:43 -0600

    Update ribbonDielectric.m

--- ribbonDielectric.m
+++ ribbonDielectric.m
@@ -31,7 +31,7 @@
 %   University of Minnesota
 %
 % Version:
-%   21 October 2020
+%   26 October 2020
 %------------------------------------------------------------------------------
 
 function meanVarRib = ribbonDielectric(csvPath,d,n)
@@ -72,13 +72,13 @@ function meanVarRib = ribbonDielectric(csvPath,d,n)
     V = utmR(2,:);
     U = utmR(1,:);
     
-    %Find midpoint of the northing values to bring them to the easting-axis
+    %Find midpoint of the northing values to bring them to the U-axis
     midpointV = (max(utmR(2,:)) + min((utmR(2,:))))/2;
     midpointU = (max(utmR(1,:)) + min((utmR(1,:))))/2;
     V = V - midpointV;
     U = U - midpointU;
     
-    %Checks that the rectangle has length axis along easting axis
+    %Checks that the rectangle has length axis along U-axis
     if (max(V) - min(V)) > 10
         theta = theta - pi/2;
         R = [cos(theta), -sin(theta); sin(theta), cos(theta)];
@@ -92,7 +92,7 @@ function meanVarRib = ribbonDielectric(csvPath,d,n)
         U = U - midpointU;
     end
     
-    %Checks that the dataset begins in -easting and ends in +easting
+    %Checks that the dataset begins in -U and ends in +U
     if U(1) > U(end)
         theta = theta - pi;
         R = [cos(theta), -sin(theta); sin(theta), cos(theta)];
@@ -122,14 +122,17 @@ function meanVarRib = ribbonDielectric(csvPath,d,n)
         end
     end
     
+    %Find offset distance from centerline and convert to double
     offsetA = strsplit(offsets(1),{'R','L'});
     offsetC = strsplit(offsets(3),{'R','L'});
-    
     offsetDA = str2double(offsetA(1));
     offsetDC = str2double(offsetC(1));
     
+    %Boolean to find which offset is greater (i.e. further from centerline)
     AoverC = offsetDA > offsetDC;
     
+    %Rereates the ribbons vector to staisfy the condition that the
+    %centerline is closer to the sensor with the smaller offset.
     if AoverC | ~isempty(d)
         if abs(ribbons(end)-V(1)) > abs(ribbons(end)-V(2*length(A)+1))
             ribbons = (min(cornersR(2,:)):d:max(cornersR(2,:)));
@@ -158,6 +161,7 @@ function meanVarRib = ribbonDielectric(csvPath,d,n)
         meanRib(i) = mean(ribbonData(:,3));
         nPoints(i) = length(ribbonData);
         
+        
         %Create array for plotting the ribbon boundaries
         ribbonLine = [min(utmR(1,:)) max(utmR(1,:)); ribbons(i) ribbons(i)];
         ribbonLine(2,:) = ribbonLine(2,:) + midpointV;
@@ -167,55 +171,20 @@ function meanVarRib = ribbonDielectric(csvPath,d,n)
         clear ribbonData; clear h; clear g; clear N; clear ribbonLine;
     end
     
+    %Plot the final ribbon divider
     ribbonLine = [min(utmR(1,:)) max(utmR(1,:)); ribbons(end) ribbons(end)];
     ribbonLine(2,:) = ribbonLine(2,:) + midpointV;
     ribbonLine = Rinv * ribbonLine;
     plot(ribbonLine(1,:),ribbonLine(2,:),'-r');
     
+    %Plot the centerline
     ribbonLine = [min(utmR(1,:)) max(utmR(1,:)); ribbons(1) ribbons(1)];
     ribbonLine(2,:) = ribbonLine(2,:) + midpointV;
     ribbonLine = Rinv * ribbonLine;
     plot(ribbonLine(1,:),ribbonLine(2,:),'-g');
     
-    
-%     if AoverC
-%         if abs(ribbons(end)-V(1)) > abs(ribbons(end)-V(2*length(A)+1))
-%             ribbonLine = [min(utmR(1,:)) max(utmR(1,:)); ribbons(end) ribbons(end)];    
-%             ribbonLine(2,:) = ribbonLine(2,:) + midpointV;
-%             ribbonLine = Rinv * ribbonLine;
-%             plot(ribbonLine(1,:),ribbonLine(2,:),'-g');
-%            ribbons = abs(ribbons - ribbons(end));
-%             ribbons = flip(ribbons);
-%             meanRib = flip(meanRib);
-%             varRib = flip(varRib);
-%             
-%         else
-%             ribbonLine = [min(utmR(1,:)) max(utmR(1,:)); ribbons(1) ribbons(1)];      
-%             ribbonLine(2,:) = ribbonLine(2,:) + midpointV;
-%             ribbonLine = Rinv * ribbonLine;
-%             plot(ribbonLine(1,:),ribbonLine(2,:),'-g');
-%             ribbons = abs(ribbons - ribbons(1));
-%             
-%         end
-%     else
-%         if abs(ribbons(end)-V(1)) < abs(ribbons(end)-V(2*length(A)+1))
-%             ribbonLine = [min(utmR(1,:)) max(utmR(1,:)); ribbons(end) ribbons(end)];
-%             ribbonLine(2,:) = ribbonLine(2,:) + midpointV;
-%             ribbonLine = Rinv * ribbonLine;
-%             plot(ribbonLine(1,:),ribbonLine(2,:),'-g');
-%             ribbons = abs(ribbons - ribbons(end));
-%             ribbons = flip(ribbons);
-%             meanRib = flip(meanRib);
-%             varRib = flip(varRib);
-%             
-%         else
-%             ribbonLine = [min(utmR(1,:)) max(utmR(1,:)); ribbons(1) ribbons(1)];
-%             ribbonLine(2,:) = ribbonLine(2,:) + midpointV;
-%             ribbonLine = Rinv * ribbonLine;
-%             plot(ribbonLine(1,:),ribbonLine(2,:),'-g');
-%             ribbons = abs(ribbons - ribbons(1));
-%         end
-%     end
+%Change ribbon distances in refference to the centerline and construct
+%outpu matrix
 ribbons = abs(ribbons(1,:)-ribbons(1));
 meanVarRib = [ribbons*3.28; 0,meanRib; 0,varRib; 0,nPoints];
     hold off
